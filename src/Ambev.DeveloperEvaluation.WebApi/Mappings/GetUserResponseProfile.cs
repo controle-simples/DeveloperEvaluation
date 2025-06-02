76cba66df3930931ea1277a057cb2e89b28ba3ad
controle-simples <controleesimpless@gmail.com>
2025-06-01 21:54:20 -0300

    enable JWT token generation and user authentication
    - Updated database path to allow correct migrations
    - Added AutoMapper mappings for AuthenticateUser and GetUser responses
    - Implemented authentication endpoint to return valid JWT token
    - Confirmed user creation and retrieval are working as expected

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Mappings/GetUserResponseProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Ambev.DeveloperEvaluation.Application.Users.GetUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings;
+
+public class GetUserResponseProfile : Profile
+{
+    public GetUserResponseProfile()
+    {
+        CreateMap<GetUserResult, GetUserResponse>();
+    }
+}
